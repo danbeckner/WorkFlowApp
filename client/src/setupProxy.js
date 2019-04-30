7de417e27fc11747ef0091b935db90c7a2c6a733
Dan Beckner <danbeckner@Dans-MacBook-Pro.local>
2019-04-30 09:51:29 -0400

    Added setupProxy.js for http-proxy-middleware

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+// const proxy = require('http-proxy-middleware');
+
+// module.exports = function(app) {
+//   app.use(proxy('/api', { 
+//         target: 'http://localhost:8081/',
+//         // changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+//         // logLevel: 'debug'
+//       }
+//       ));
+// };
